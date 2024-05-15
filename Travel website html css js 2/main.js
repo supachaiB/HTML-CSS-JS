9c3edc137d73b03f9a96e6145595f7f92ad0cd73
supachaiB <96191074+supachaiB@users.noreply.github.com>
2024-05-15 14:32:50 +0700

    14:32
    time 39:28 end -> section popular

---
+++ Travel website html css js 2/main.js
@@ -0,0 +1,49 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+/*=============== MENU SHOW ===============*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+};
+
+/*=============== MENU HIDDEN ===============*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+};
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav_link, we remove the show-menu
+    navMenu.classList.remove('show-menu')
+};
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // when the scroll is greater than 50 viewport height, add the s-header class
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)    
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
